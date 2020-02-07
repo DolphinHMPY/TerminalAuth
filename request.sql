15365d181a02e3cc4eb0b3dbc7d8ae3f51862094
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-02-07 17:09:48 +0300

    Update request.sql

--- request.sql
+++ request.sql
@@ -1,33 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Фев 07 2020 г., 16:11
--- Версия сервера: 10.3.13-MariaDB
--- Версия PHP: 7.1.22
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `python`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `social_terminal`
---
-
 CREATE TABLE `social_terminal` (
   `TABLE_ID` int(11) NOT NULL,
   `NAME` text NOT NULL,
@@ -36,28 +6,6 @@ CREATE TABLE `social_terminal` (
   `START_ID` text NOT NULL,
   `NOW_ID` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `social_terminal`
---
-ALTER TABLE `social_terminal`
-  ADD PRIMARY KEY (`TABLE_ID`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `social_terminal`
---
 ALTER TABLE `social_terminal`
   MODIFY `TABLE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
